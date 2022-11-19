1e0c5ee51748ed7c832dc9cc857241927e09f1a4
Mike Elzas <mikeelzas@gmail.com>
2022-11-19 15:20:29 +0100

    updating app

--- app/app.py
+++ app/app.py
@@ -3,37 +3,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-from google.oauth2 import service_account
-from google.cloud import bigquery
+#from google.oauth2 import service_account
+#from google.cloud import bigquery
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+#credentials = service_account.Credentials.from_service_account_info(
+#    st.secrets["gcp_service_account"]
+#)
 
+#client = bigquery.Client(credentials=credentials)
 
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
-
-# Print results.
-st.write("Some wise words from Shakespeare:")
-for row in rows:
-    st.write("✍️ " + row['word'])
 
 st.markdown("""# This is a header
 ## This is a sub header
