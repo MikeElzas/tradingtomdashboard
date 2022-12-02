c7eefea7fe1c7fe003095362a345e98bfb7a88a3
Mike Elzas <mikeelzas@gmail.com>
2022-12-02 11:23:12 +0100

    updating the dashboard

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from htbuilder import div, big, h2, styles
 from htbuilder.units import rem
 import altair as alt
+import random
 
 from google.oauth2 import service_account
 from google.cloud import bigquery
@@ -44,12 +45,12 @@ def display_dial(title, value, color):
 ############################################
 
 #setting page config
-st.set_page_config(layout="wide", page_title="Trading Tom invester dashboard", page_icon=":rocket:")
+st.set_page_config(layout="wide", page_title="Trading Tom investor dashboard", page_icon=":rocket:")
 
 row1_1, row1_2 = st.columns((2, 2))
 
 with row1_1:
-    st.title("Trading Tom invester dashboard")
+    st.title("Trading Tom investor dashboard")
 
 with row1_2:
     st.write(
@@ -60,11 +61,25 @@ with row1_2:
    """
     )
 
-# choose which ticker is displayed
-ticker = st.sidebar.selectbox(
-    'Coin to Plot',
-    options = ['Bitcoin', 'Ethereum', 'Solana']
-)
+
+
+#Creating boxes for the Coin to plot and the days to plot
+row3_1, row3_2 = st.columns((2))
+
+#choose which coin to plot:
+with row3_1:
+    ticker = st.selectbox(
+        'Coin to Plot',
+        options = ['Bitcoin', 'Ethereum', 'Solana']
+    )
+#choose the amount of days displayed
+with row3_2:
+    days_to_plot = st.slider(
+    'Days to Plot',
+    min_value = 1,
+    max_value = 365,
+    value = 120,
+    )
 
 
 #changing the coin into a ticker
@@ -75,30 +90,40 @@ elif ticker == "Ethereum":
 else:
     ticker_plot = "SOL_USDT"
 
-
 table_ref = dataset_ref.table(ticker_plot)
 table = client.get_table(table_ref)
-
-#choose the amount of days displayed in the chart
-days_to_plot = st.sidebar.slider(
-    'Days to Plot',
-    min_value = 1,
-    max_value = 300,
-    value = 120,
-)
 
 #Manipulating data
 data = client.list_rows(table).to_dataframe()
 data = data.sort_values(by = "datetime")
 head = data.head()
 
-
-#using the slider variabole to determine how much data the chart highlights
+#using the slider variabel to determine how much data the chart highlights
 data_chart = data.tail(days_to_plot * 24)
 
 data_chart["return_coin"] = data_chart["close"].pct_change()*100
-data_chart["return_coin_cum"] = data_chart["return_coin"].cumsum()
-data_chart["return_port"] = -2
+data_chart["Coin return"] = data_chart["return_coin"].cumsum()
+data_chart["port_return"] = 0
+
+#calculating the return on the porftolio
+for i in range(data_chart.shape[0]):
+    data_chart["port_return"].loc[i] = random.uniform(-0.5, 0.5)
+
+data_chart["Portfolio return"] = data_chart["port_return"].cumsum()
+
+latest_return = round(data_chart["Portfolio return"].iloc[-1],1)
+latest_return_amount = round(((1 + (latest_return/100) * 100000)+100000))
+
+st.write("## Overview of invest capital, current capital and the return on capital")
+
+row2_1, row2_2, row2_3 = st.columns((3))
+
+with row2_1:
+    display_dial("Invested amount", f"100000", COLOR_BLUE)
+with row2_2:
+    display_dial("Current amount", f"{latest_return_amount}", COLOR_CYAN)
+with row2_3:
+    display_dial(f"% return", f"{latest_return}", COLOR_BLUE)
 
 
 
@@ -115,25 +140,10 @@ fig.update_layout(
 
 )
 
-
-#setting up middle section of the dashboard
-
-st.write("## Overview of invest capital, current capital and the return on capital")
-
-row2_1, row2_2, row2_3 = st.columns((3))
-
-with row2_1:
-    display_dial("Invested amount", f"100K", COLOR_BLUE)
-with row2_2:
-    display_dial("Current amount", f"120K", COLOR_CYAN)
-with row2_3:
-    display_dial(f"% return", f"20%", COLOR_BLUE)
-
-
 c= alt.Chart(data_chart).mark_line().encode(
     x=alt.X('datetime:T', axis=alt.Axis(tickCount= 12 )),
     y=alt.Y(alt.repeat("layer"), type= "quantitative"),
-    color=alt.ColorDatum(alt.repeat('layer'))).repeat(layer= ["return_coin_cum", "return_port"])
+    color=alt.ColorDatum(alt.repeat('layer'))).repeat(layer= ["Coin return", "Portfolio return"])
 
 st.write(f"""**Comparison of the portfolio return against the {ticker} return**""")
 st.altair_chart(c, use_container_width= True)
